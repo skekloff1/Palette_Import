b82643ae6300248b7ab47480ee28c1b494a8b9e9
Goro <goro@iMac.local>
2018-11-29 09:27:47 -0500

    adding additional comments

--- palette_import_lib.py
+++ palette_import_lib.py
@@ -6,6 +6,8 @@ and put it into a dictionary
 
 def read_palette_from_file(palette_filename):
     """
-    read the palette from the csv file and put into a dictionary
+    read the palette from the csv file.
+    Each row is a key and an RGB triple.  Make that RGB triple
+    into a TUPLE and then put into a dictionary with the key.
     """
     pass
